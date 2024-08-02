c7cd4c09eaf4bd72ecd15c0f14544ab259a7dd18
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-08-02 15:28:36 -0300

    Add health check endpoint and update dependencies
    Implemented a new health check endpoint to monitor database and cache status, along with application version and environment. Enhanced integration tests to verify the new endpoint's response. Updated `composer.json` and `composer.lock` with necessary dependencies including `pestphp/pest-plugin-watch`.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,51 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/up', function () {
+    $databaseConnection = true;
+    $databaseVersion = '';
+    $status = 'OK';
+
+
+    // Verifica a conexão com o banco de dados
+    try {
+        $databaseVersion = DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
+
+    } catch (\Exception $exception) {
+        $databaseConnection = false;
+        $status = false;
+    }
+
+    // Verifica a conexão com o cache
+    try {
+        Cache::store()->get('key');
+        $cacheConnection = true;
+    } catch (\Exception $e) {
+        $cacheConnection = false;
+        $status = false;
+    }
+
+    return response()->json([
+        'status' => $status,
+        'database' => [
+            'connected' => $databaseConnection,
+            'version' => $databaseVersion,
+        ],
+        'cache' => [
+            'connected' => $cacheConnection,
+        ],
+        'app' => [
+            'version' => app()->version(),
+            'environment' => app()->environment(),
+        ],
+    ]);
+
 });
--- tests/IntegrationTests/UserIntegrationTest.php
+++ tests/IntegrationTests/UserIntegrationTest.php
@@ -7,5 +7,19 @@ uses(Tests\TestCase::class, RefreshDatabase::class);
 test('health check', function () {
     $response = $this->get('/up');
 
-    $response->assertStatus(200);
+    $response->assertStatus(200)
+        ->assertJson([
+            'status' => 'OK',
+            'database' => [
+                'connected' => true,
+                'version' => '8.0.32',
+            ],
+            'cache' => [
+                'connected' => true,
+            ],
+            'app' => [
+                'version' => '11.19.0',
+                'environment' => 'testing',
+            ],
+        ]);
 });
